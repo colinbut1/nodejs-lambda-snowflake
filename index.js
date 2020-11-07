940770b9979a2ad0faae845380be056dd2d242b3
Colin But <colin.but@sainsburys.co.uk>
2020-11-07 11:40:34 +0000

    feat: using async to retrieve from ssm

--- index.js
+++ index.js
@@ -1,71 +1,63 @@
 var snowflake = require('snowflake-sdk')
 var AWS = require('aws-sdk')
-var ssm = new AWS.SSM();
 
-var usernameParams = {
-    Name: '/snowflake/service-user/username',
-    WithDecryption: false
-};
+exports.handler = async(event, context) => {
+    var ssm = new AWS.SSM();
 
-var passwordParams = {
-    Name: '/snowflake/service-user/password',
-    WithDecryption: true
-}
+    var usernameParams = {
+        Name: '/snowflake/service-user/username',
+        WithDecryption: false
+    };
 
-var snowflakeUsername;
-ssm.getParameter(usernameParams, function(err, data){
-    if (err)    console.error(err, err.stack);
-    else        snowflakeUsername = data
-});
-
-var snowflakePassword;
-ssm.getParameter(passwordParams, function(err, data){
-    if (err)    console.error(err, err.stack);
-    else        snowflakePassword = data
-});
-
-console.log('Fetched snowflake credentials from SSM, [username: ' + snowflakeUsername + ', password: ' + snowflakePassword + ']');
-
-var connection = snowflake.createConnection({
-    account: 'js_bank.eu-west-1.privatelink',
-    username: snowflakeUsername,
-    password: snowflakePassword,
-    warehouse: 'JS_BANK',
-    schema: 'AQM',
-    database: 'AQM'
-});
-
-connection.connect(
-    function(err, conn) {
-        if (err) {
-            console.error('Unable to connect to Snowflake: ' + err.message);
-        } else {
-            console.log('Successfully connected to Snowflake.');
-        }
+    var passwordParams = {
+        Name: '/snowflake/service-user/password',
+        WithDecryption: true
     }
-);
 
-var SQL_COMMAND = 'SELECT 1';
+    const snowflakeUsername = await ssm.getParameter(usernameParams).promise();
+    const snowflakePassword = await ssm.getParameter(passwordParams).promise();
 
-var statement = connection.execute({
-    sqlText: SQL_COMMAND,
-    complete: function(err, stmt, rows) {
-        if (err) {
-            console.error('Failed to execute statement due to the following error: ' + err.message);
-        } else {
-            console.log('Successfully executed SQL statement: ' + stmt.getSqlText());
-            console.log('Rows returned: ' + rows.length);
-            for (var i = 0; i < rows.length - 1; i++) {
-                console.log(rows[i]);
+    var connection = snowflake.createConnection({
+        account: 'js_bank.eu-west-1.privatelink',
+        username: snowflakeUsername,
+        password: snowflakePassword,
+        warehouse: 'JS_BANK',
+        schema: 'AQM',
+        database: 'AQM'
+    });
+
+    connection.connect(
+        function(err, conn) {
+            if (err) {
+                console.error('Unable to connect to Snowflake: ' + err.message);
+            } else {
+                console.log('Successfully connected to Snowflake.');
             }
         }
-    }
-});
+    );
 
-connection.destroy(function(err, conn){
-    if (err) {
-        console.error('Unable to disconnect: ' + err.message);
-    } else {
-        console.log('Disconnected connection with id: ' + connection.getId());
-    }
-});
+    var SQL_COMMAND = 'SELECT 1';
+
+    var statement = connection.execute({
+        sqlText: SQL_COMMAND,
+        complete: function(err, stmt, rows) {
+            if (err) {
+                console.error('Failed to execute statement due to the following error: ' + err.message);
+            } else {
+                console.log('Successfully executed SQL statement: ' + stmt.getSqlText());
+                console.log('Rows returned: ' + rows.length);
+                for (var i = 0; i < rows.length - 1; i++) {
+                    console.log(rows[i]);
+                }
+            }
+        }
+    });
+
+    connection.destroy(function(err, conn){
+        if (err) {
+            console.error('Unable to disconnect: ' + err.message);
+        } else {
+            console.log('Disconnected connection with id: ' + connection.getId());
+        }
+    });
+}
